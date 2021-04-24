c279407825b4abe52bf3d72eb0bd779b57bc7839
ccaruso <elcaru88@gmail.com>
2021-04-24 19:10:38 -0300

    Added missing PrintService

---
+++ BowlingScoreService/src/main/java/com/codechallenge/service/PrintService.java
@@ -0,0 +1,9 @@
+package com.codechallenge.service;
+
+import com.codechallenge.model.Board;
+
+public interface PrintService {
+
+    String printBoard(Board board);
+
+}
---
+++ BowlingScoreService/src/main/java/com/codechallenge/service/impl/PrintServiceImpl.java
@@ -0,0 +1,69 @@
+package com.codechallenge.service.impl;
+
+import com.codechallenge.model.Board;
+import com.codechallenge.model.Frame;
+import com.codechallenge.model.Player;
+import com.codechallenge.service.PrintService;
+
+public class PrintServiceImpl implements PrintService {
+
+    @Override
+    public String printBoard(Board board){
+        StringBuilder boardString = new StringBuilder();
+        appendToBoardFrameTitle(boardString);
+        board.getPlayers().forEach( player -> appendToBoardSinglePlayer(boardString, player));
+        return boardString.toString();
+    }
+
+    private void appendToBoardFrameTitle(StringBuilder boardString) {
+        boardString.append("Frame\t\t1\t\t2\t\t3\t\t4\t\t5\t\t6\t\t7\t\t8\t\t9\t\t10");
+        boardString.append(System.getProperty("line.separator"));
+    }
+
+    private void appendToBoardSinglePlayer(StringBuilder boardString, Player player) {
+        player.buildFrames();
+        boardString.append(player.getName());
+        boardString.append(System.getProperty("line.separator"));
+        boardString.append("Pinfalls\t");
+        player.getFrames().forEach(frame -> appendToBoardPinFalls(boardString, frame));
+        boardString.append(System.getProperty("line.separator"));
+        Integer acumScore = 0;
+        boardString.append("Score\t\t");
+        for (Frame singleFrame : player.getFrames()) {
+            acumScore = appendToBoardScore(boardString, singleFrame, acumScore);
+        }
+        boardString.append(System.getProperty("line.separator"));
+    }
+
+    private Integer appendToBoardScore(StringBuilder boardString, Frame frame, Integer acumScore) {
+        Integer currentScore = acumScore + frame.getScore();
+        boardString.append(currentScore + "\t\t");
+        return currentScore;
+    }
+
+    private void appendToBoardPinFalls(StringBuilder boardString, Frame frame) {
+        if (frame.isLast()) {
+            if (frame.isStrike()) {
+                boardString.append("\tX\t");
+                boardString.append(frame.getPinThrows().get(1) + "\t");
+                boardString.append(frame.getPinThrows().get(2));
+            } else if (frame.isSpare()) {
+                boardString.append(frame.getPinThrows().get(0) + "\t");
+                boardString.append("/\t");
+                boardString.append(frame.getPinThrows().get(2));
+            } else {
+                boardString.append(frame.getPinThrows().get(0) + "\t");
+                boardString.append(frame.getPinThrows().get(1));
+            }
+        } else if (frame.isStrike()) {
+            boardString.append("\tX\t");
+        } else if (frame.isSpare()) {
+            boardString.append(frame.getPinThrows().get(0) + "\t");
+            boardString.append("/\t");
+        } else {
+            boardString.append(frame.getPinThrows().get(0) + "\t");
+            boardString.append(frame.getPinThrows().get(1) + "\t");
+        }
+    }
+
+}
